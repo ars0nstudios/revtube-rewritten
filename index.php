85aacfa1365c9c8d5ae4686247219275fa767d63
RiskDevKit <96445516+TheRealHyped@users.noreply.github.com>
2022-08-04 05:19:10 -0400

    funny sarcasm

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 <body>
     <?php include 'header.php';?>
 <div class="container">
-<h1>Sorry, we are working on this!</h1>
+<h1>Sorry, 
+<abbr title="amer1kas, nobody else works on this site sadly">we</abbr> are working on this!</h1>
 You can find more videos on <a href="trending.php">Trending</a>.
 
 <?php include 'footer.php';?>
