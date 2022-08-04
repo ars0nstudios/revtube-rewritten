d124e8a27aba2582b3c0537af8aece83f5ad5d40
RiskDevKit <96445516+TheRealHyped@users.noreply.github.com>
2022-08-04 04:51:49 -0400

    index placeholder
    read commit title

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 <body>
     <?php include 'header.php';?>
 <div class="container">
-
+<h1>Sorry, we are working on this!</h1>
+You can find more videos on <a href="trending.php">Trending</a>.
 
 <?php include 'footer.php';?>
 </div>
