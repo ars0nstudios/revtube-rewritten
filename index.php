8b9d6fb2af6298abd5c988cfb47d6e4d99687376
RiskDevKit <96445516+TheRealHyped@users.noreply.github.com>
2022-08-04 05:23:16 -0400

    time estimate

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <?php include 'header.php';?>
 <div class="container">
 <h1>Sorry, 
-<abbr title="amer1kas, nobody else works on this site sadly">we</abbr> are working on this!</h1>
+<abbr title="amer1kas, nobody else works on this site sadly">we</abbr> are working on this feature!</h1>
+Check back <abbr title="estimated: mid august 2022">later</abbr> for this.<br>
 You can find more videos on <a href="trending.php">Trending</a>.
 
 <?php include 'footer.php';?>
